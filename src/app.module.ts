5d416790a6be7eec5ec47818d82684217791e2ce
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-12-04 10:39:35 +0800

    Scaffold /bills module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BillsModule } from './bills/bills.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BillsModule],
 })
 export class AppModule {}
