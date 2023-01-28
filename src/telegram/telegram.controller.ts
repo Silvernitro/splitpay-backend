c5a6f93e6c479831be463ced9b5671ae7702e570
Lau Siaw Sam <lausiawsam98@gmail.com>
2023-01-28 11:36:58 +0800

    Add telegram module to validate webapp data

--- src/telegram/telegram.controller.ts
+++ src/telegram/telegram.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import telegramDataDto from './dto/telegram-data.dto';
 import { TelegramService } from './telegram.service';
 
 @Controller('telegram')
 export class TelegramController {
   constructor(private readonly telegramService: TelegramService) {}
+
+  @Post('validateData')
+  validateTelegramData(@Body() telegramData: telegramDataDto) {
+    return this.telegramService.validateWebAppData(telegramData.initData);
+  }
 }
--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import CryptoJS from 'crypto-js';
 
 @Injectable()
-export class TelegramService {}
+export class TelegramService {
+  constructor(private readonly configService: ConfigService) {}
+
+  validateWebAppData(initDataString: string): boolean {
+    const initData = new URLSearchParams(initDataString);
+    const hash = initData.get('hash');
+    const dataFields: string[] = [];
+
+    initData.sort();
+    initData.forEach(
+      (val, key) => key !== 'hash' && dataFields.push(`${key}=${val}`),
+    );
+
+    const dataCheckString = dataFields.join('\n');
+    const secret = CryptoJS.HmacSHA256(
+      this.configService.get<string>('TELEGRAM_BOT_TOKEN'),
+      'WebAppData',
+    );
+    const dataHash = CryptoJS.HmacSHA256(dataCheckString, secret).toString(
+      CryptoJS.enc.Hex,
+    );
+
+    return dataHash === hash;
+  }
+}
